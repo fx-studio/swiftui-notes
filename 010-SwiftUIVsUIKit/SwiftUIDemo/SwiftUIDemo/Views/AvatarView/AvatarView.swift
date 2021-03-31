65f0cb2f98b4346ab56575ffa53b04850a2f5f56
Tien Le P <tien.le@monstar-lab.com>
2021-03-31 10:32:49 +0700

    014 - Custom UIView in SwiftUI Project

---
+++ 010-SwiftUIVsUIKit/SwiftUIDemo/SwiftUIDemo/Views/AvatarView/AvatarView.swift
@@ -0,0 +1,20 @@
+//
+//  AvatarView.swift
+//  SwiftUIDemo
+//
+//  Created by lephuongtien on 3/30/21.
+//
+
+import UIKit
+
+class AvatarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ 010-SwiftUIVsUIKit/SwiftUIDemo/SwiftUIDemo/Views/AvatarView/MyAvatar.swift
@@ -0,0 +1,8 @@
+//
+//  MyAvatar.swift
+//  SwiftUIDemo
+//
+//  Created by lephuongtien on 3/30/21.
+//
+
+import Foundation
